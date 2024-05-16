e293a4f29c21f63ff3138c9986cdffcefb97debe
Dalton Carter <Dj_Carter_Gottenbos@yahoo.com>
2024-05-15 18:51:38 -0700

    Began Pre-rendering and Fetching, Screen Caps folder added.

---
+++ next-pre-rendering/pages/users.js
@@ -0,0 +1,31 @@
+
+import User from "@/components/user"
+
+export default function userList({users}) {
+    return(
+        <>
+        <h1>List of User:</h1>
+        {
+            users.map(user => {
+                return(
+                    <div key={user.id}>
+                        <User user={user} />
+                    </div>
+                )
+            })
+        }
+        </>
+    )
+}
+
+export async function getStaticProps(){
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await response.json()
+    console.log(data)
+
+    return {
+        props: {
+            users: data,
+        },
+    }
+}
